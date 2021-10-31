120b4f6a2cdce207eef927fe4c609acc429e118a
Michael Vargas Soto <michaelvargas47@hotmail.com>
2021-10-31 14:49:47 -0600

    Add project files.

---
+++ Agencia_de_Viajes/frmPrincipal.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Menu = New System.Windows.Forms.MenuStrip()
+        Me.DestinoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReservasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ComprasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExtrasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AcercaDeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Menu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Menu
+        '
+        Me.Menu.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Menu.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.Menu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DestinoToolStripMenuItem, Me.ReservasToolStripMenuItem, Me.ComprasToolStripMenuItem, Me.ExtrasToolStripMenuItem, Me.AcercaDeToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.Menu.Location = New System.Drawing.Point(0, 0)
+        Me.Menu.Name = "Menu"
+        Me.Menu.Size = New System.Drawing.Size(1124, 31)
+        Me.Menu.TabIndex = 1
+        Me.Menu.Text = "MenuStrip1"
+        '
+        'DestinoToolStripMenuItem
+        '
+        Me.DestinoToolStripMenuItem.Name = "DestinoToolStripMenuItem"
+        Me.DestinoToolStripMenuItem.Size = New System.Drawing.Size(80, 24)
+        Me.DestinoToolStripMenuItem.Text = "Destinos"
+        '
+        'ReservasToolStripMenuItem
+        '
+        Me.ReservasToolStripMenuItem.Name = "ReservasToolStripMenuItem"
+        Me.ReservasToolStripMenuItem.Size = New System.Drawing.Size(80, 24)
+        Me.ReservasToolStripMenuItem.Text = "Reservas"
+        '
+        'ComprasToolStripMenuItem
+        '
+        Me.ComprasToolStripMenuItem.Name = "ComprasToolStripMenuItem"
+        Me.ComprasToolStripMenuItem.Size = New System.Drawing.Size(82, 24)
+        Me.ComprasToolStripMenuItem.Text = "Compras"
+        '
+        'ExtrasToolStripMenuItem
+        '
+        Me.ExtrasToolStripMenuItem.Name = "ExtrasToolStripMenuItem"
+        Me.ExtrasToolStripMenuItem.Size = New System.Drawing.Size(62, 24)
+        Me.ExtrasToolStripMenuItem.Text = "Extras"
+        '
+        'AcercaDeToolStripMenuItem
+        '
+        Me.AcercaDeToolStripMenuItem.Name = "AcercaDeToolStripMenuItem"
+        Me.AcercaDeToolStripMenuItem.Size = New System.Drawing.Size(89, 24)
+        Me.AcercaDeToolStripMenuItem.Text = "Acerca de"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(63, 27)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1124, 652)
+        Me.Controls.Add(Me.Menu)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.Menu
+        Me.Name = "frmPrincipal"
+        Me.Text = "Menu Viajando y conociendo"
+        Me.Menu.ResumeLayout(False)
+        Me.Menu.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Menu As MenuStrip
+    Friend WithEvents DestinoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReservasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ComprasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExtrasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AcercaDeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Agencia_de_Viajes/frmPrincipal.vb
@@ -0,0 +1,5 @@
+﻿Public Class frmPrincipal
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+End Class
